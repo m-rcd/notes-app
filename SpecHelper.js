bad43120655d0f57ccbf4a8cbfa154cc3423f3e8
Marianne Rachid <marianne.rachid@gmail.com>
2018-10-17 20:47:16 +0100

    add more test

--- SpecHelper.js
+++ SpecHelper.js
@@ -8,11 +8,29 @@ const it = (msg, fn) => describe('  ' + msg, fn)
 const matchers = (exp) => ({
   toBe: (asssertion) => {
     if (exp === assertion) {
-      console.log('Pass!')
+      console.log('%c Test passed!', "color: green")
       return true
     } else {
       console.log('Fail!')
       return false
+    }
+  },
+
+  toContain(element) => {
+    if (exp.includes(element)) {
+      console.log('%c Test passed! '+ exp + 'includes ' + element + ' !', "color: green")
+      return true
+    } else {
+      console.log('Fail' + exp + 'does not include ' + element + ' !')
+      return false
+    }
+  },
+
+  notToContain(element) => {
+    if (exp.includes(element)) {
+      console.log('Fail' + exp + 'includes ' + element + ' !')
+    } else {
+      console.log('%c Test passed! '+ exp + 'does not include ' + element + ' !', "color: green")
     }
   }
 })
