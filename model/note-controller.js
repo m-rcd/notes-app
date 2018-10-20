0f95c8b941d7c1d807eaf1495ceb8aa7bb882f9b
Marianne Rachid <marianne.rachid@gmail.com>
2018-10-20 08:59:42 +0100

    add noet

--- model/note-controller.js
+++ model/note-controller.js
@@ -3,7 +3,6 @@
 (function(exports){
   function NoteController(list = new NoteList(),  listView = NoteListView) {
     this.list = list;
-    this.list.create('Favourite Harry potter character: Hedwig')
     this.view = new listView(list)
   }
 
